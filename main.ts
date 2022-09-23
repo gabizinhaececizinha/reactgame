479a7720afbe49af73938d803c3e75d148cc4476
gabizinhaececizinha <114228563+gabizinhaececizinha@users.noreply.github.com>
2022-09-23 08:00:59 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("vamos jogar, pussi")
+    basic.clearScreen()
+    basic.pause(randint(1000, 10000))
+    basic.showIcon(IconNames.Yes)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        . . . # #
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+})
+basic.forever(function () {
+	
+})
